a2d9e54353503e218c26810d11db46de33cb82c8
suraj kumar <ravi.suraj110@gmail.com>
2017-07-07 15:37:28 +0530

    Add Ueberauth using Github

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Discusss.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Discusss, []},
-     extra_applications: [:logger]]
+     extra_applications: [:logger, :ueberauth_github]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule Discusss.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"}, 
+     {:ueberauth_github, "~> 0.4.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
---
+++ web/controllers/plugs/set_user.ex
@@ -0,0 +1,22 @@
+defmodule Discusss.Plugs.SetUser do
+  import Plug.Conn
+  
+  alias Discusss.Repo
+  alias Discusss.User
+
+  def init(_opts) do
+    
+  end
+
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Repo.get(User, user_id) -> 
+        assign(conn, :user, user)
+      true -> 
+        assign(conn, :user, nil)
+
+    end
+  end
+end
